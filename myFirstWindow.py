797d382dc89c2c9ce27e9955c8649871bf78e3e6
unknown <99067235@mydavinci.nl>
2022-02-24 14:50:14 +0100

    commit

--- myFirstWindow.py
+++ myFirstWindow.py
@@ -1,8 +1,14 @@
 from tkinter import *
 import tkinter as tk
+import time
+
+
 
 window = tk.Tk()
-
-window.mainloop()
-
+window.title("My First Window")
 window.geometry("50x50")
+time.sleep(2)
+window.geometry("100x100")
+time.sleep(2)
+window.geometry("150x150")
+window.mainloop()
