f09308d897b6daaed9e2106e59be31f88e5e4bc5
99067235 <99067235@mydavinci.nl>
2022-03-02 15:06:33 +0100

    bezig met clock

--- clock.py
+++ clock.py
@@ -1,6 +1,60 @@
+from cgitb import text
 import tkinter as tk
+import time
+from tkinter.tix import TEXT
+from typing import Text
+
 
 window = tk.Tk()
 window.configure(bg= "black")
 window.geometry("500x500")
+
+
+uren = 0
+minuten = 0
+seconden = 0
+
+timer = uren, ":", minuten, ":", seconden
+
+
+
+box1 = tk.Label(
+    window,
+    text="TIMER:",
+    bg="black",
+    fg="white",
+)
+
+box1.pack(
+    ipadx=10,
+    ipady=10
+)
+
+box2 = tk.Label(
+    window,
+    text=timer,
+    bg="black",
+    fg="white",
+)
+
+box2.pack(
+    ipadx=10,
+    ipady=10
+)
+
 window.mainloop()
+
+def secondenTimer():
+    i = 0
+    global seconden, minuten    
+    for i in range(59):
+        time.sleep(1)
+        seconden += 1
+        
+    time.sleep(1)
+    seconden = 0
+    minuten += 1
+    print(uren, ":", minuten, ":", seconden)
+    secondenTimer()
+
+secondenTimer()
